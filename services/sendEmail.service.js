9faa5f905228813162c6bb5194e585507bdcb09e
Sahil Pinjari <sahilpinjari51209@gmail.com>
2026-05-01 17:02:08 +0530

    Changed Email Method from SMTP to brevo-api-key

--- services/sendEmail.service.js
+++ services/sendEmail.service.js
@@ -1,32 +1,31 @@
+import SibApiV3Sdk from "sib-api-v3-sdk";
 import { ApiError } from "../utils/ApiError.js";
-import nodemailer from "nodemailer";
 
-export const sendEmail = async ({
-  from = "<noreply@skayshare.com>",
-  to,
-  subject,
-  htmlTamplete,
-}) => {
+export const sendEmail = async ({ to, subject, htmlTamplete }) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAILE_HOSTER,
-      port: process.env.MAILE_HOSTER_PORT,
-      auth: {
-        user: process.env.MAILE_HOST_USER,
-        pass: process.env.MAILE_HOST_PASS,
-      },
-    });
+    const client = SibApiV3Sdk.ApiClient.instance;
 
-    let info = {
-      from: from,
-      to: to,
-      subject: subject,
-      html: htmlTamplete,
+    const apiKey = client.authentications["api-key"];
+    apiKey.apiKey = process.env.BREVO_API_KEY;
+
+    const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+
+    const sender = {
+      email: "noreply@skayshare.com",
+      name: "SkayShare",
     };
 
-    await transporter.sendMail(info);
+    const receivers = [{ email: to }];
+
+    await tranEmailApi.sendTransacEmail({
+      sender,
+      to: receivers,
+      subject,
+      htmlContent: htmlTamplete,
+    });
+
   } catch (err) {
-    console.error("❌ Unable to Send Email!...:", err.message);
-    throw new ApiError(500, "Unable to Send Email!...");
+    console.error("❌ Email Error:", err.response?.body || err.message);
+    throw new ApiError(500, "Unable to Send Email");
   }
 };
